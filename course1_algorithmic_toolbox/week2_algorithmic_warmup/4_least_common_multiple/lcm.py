283c09ea2e4cdb1f207791690dbffce9a777f9f1
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-04-16 05:25:20 +0530

    Solve w1q4, w1q5

--- course1_algorithmic_toolbox/week2_algorithmic_warmup/4_least_common_multiple/lcm.py
+++ course1_algorithmic_toolbox/week2_algorithmic_warmup/4_least_common_multiple/lcm.py
@@ -1,15 +1,19 @@
 # Uses python3
 import sys
 
-def lcm_naive(a, b):
-    for l in range(1, a*b + 1):
-        if l % a == 0 and l % b == 0:
-            return l
 
-    return a*b
+def gcd(a, b):
+    if b == 0:
+        return a
+    a1 = a % b
+    return gcd(b, a1)
+
+
+def lcm_naive(a, b):
+    return a * b // gcd(a, b)
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     a, b = map(int, input.split())
     print(lcm_naive(a, b))
-
--- course1_algorithmic_toolbox/week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.py
+++ course1_algorithmic_toolbox/week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.py
@@ -1,19 +1,30 @@
 # Uses python3
 import sys
 
-def get_fibonacci_huge_naive(n, m):
-    if n <= 1:
-        return n
 
-    previous = 0
-    current  = 1
+def get_fibonacci_huge(n, m):
+    fib_arr = [0, 1]
+    period = [0, 1]
 
-    for _ in range(n - 1):
-        previous, current = current, previous + current
+    i = 2
+    while True:
+        fib_i = fib_arr[i - 1] + fib_arr[i - 2]
+        period_remainder_i = fib_i % m
 
-    return current % m
+        if period_remainder_i == 1 and period[i - 1] == 0:
+            fib_arr.pop()
+            period.pop()
+            break
+
+        fib_arr.append(fib_i)
+        period.append(fib_i % m)
+        i += 1
+
+    n1 = n % len(period)
+    return period[n1]
+
 
 if __name__ == '__main__':
-    input = sys.stdin.read();
+    input = sys.stdin.read()
     n, m = map(int, input.split())
-    print(get_fibonacci_huge_naive(n, m))
+    print(get_fibonacci_huge(n, m))
