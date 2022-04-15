c5add9d19a60573603453bcdca02774a6a017d89
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-04-16 03:28:59 +0530

    Solve w2q1

--- course1_algorithmic_toolbox/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ course1_algorithmic_toolbox/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -1,9 +1,12 @@
 # Uses python3
 def calc_fib(n):
-    if (n <= 1):
-        return n
+    arr = [None for i in range(n + 1)]
+    arr[0] = 0
+    arr[1] = 1
+    for i in range(2, n + 1):
+        arr[i] = arr[i - 1] + arr[i - 2]
+    return arr[n]
 
-    return calc_fib(n - 1) + calc_fib(n - 2)
 
 n = int(input())
 print(calc_fib(n))
