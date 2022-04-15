992810b5155a151bb6447f4e94b86f727d6783fc
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-04-16 03:39:07 +0530

    Refactor w2q2

--- course1_algorithmic_toolbox/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
+++ course1_algorithmic_toolbox/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
@@ -2,7 +2,7 @@
 import sys
 
 
-def get_fibonacci_last_digit_naive(n):
+def get_fibonacci_last_digit(n):
     if n <= 1:
         return n
 
@@ -18,4 +18,4 @@ def get_fibonacci_last_digit_naive(n):
 if __name__ == '__main__':
     input = sys.stdin.read()
     n = int(input)
-    print(get_fibonacci_last_digit_naive(n))
+    print(get_fibonacci_last_digit(n))
