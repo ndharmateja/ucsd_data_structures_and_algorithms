19c370c728ecd16cd50c60ccc0f484ba16ff460b
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-04-23 19:47:32 +0530

    Solve w4q1

--- course1_algorithmic_toolbox/week4_divide_and_conquer/1_binary_search/binary_search.py
+++ course1_algorithmic_toolbox/week4_divide_and_conquer/1_binary_search/binary_search.py
@@ -1,5 +1,18 @@
 def binary_search(keys, query):
     # write your code here
+    return binary_search_helper(keys, 0, len(keys) - 1, query)
+
+
+def binary_search_helper(keys, low, high, query):
+    if high < low:
+        return -1
+    mid = low + (high - low) // 2
+    if keys[mid] == query:
+        return mid
+    elif keys[mid] > query:
+        return binary_search_helper(keys, low, mid - 1, query)
+    else:
+        return binary_search_helper(keys, mid + 1, high, query)
 
 
 if __name__ == '__main__':
@@ -13,3 +26,9 @@ if __name__ == '__main__':
 
     for q in input_queries:
         print(binary_search(input_keys, q), end=' ')
+
+    # print(binary_search([1, 5, 8, 12, 13], 8))
+    # print(binary_search([1, 5, 8, 12, 13], 1))
+    # print(binary_search([1, 5, 8, 12, 13], 23))
+    # print(binary_search([1, 5, 8, 12, 13], 1))
+    # print(binary_search([1, 5, 8, 12, 13], 11))
