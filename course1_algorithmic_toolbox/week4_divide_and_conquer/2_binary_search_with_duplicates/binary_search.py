3b5f6ef0f5551454beec17cd55126ffd644d53d0
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-04-24 13:40:38 +0530

    Solve w4q2

--- course1_algorithmic_toolbox/week4_divide_and_conquer/2_binary_search_with_duplicates/binary_search.py
+++ course1_algorithmic_toolbox/week4_divide_and_conquer/2_binary_search_with_duplicates/binary_search.py
@@ -1,6 +1,22 @@
-def binary_search(keys, query):
-    # write your code here
-    pass
+def binary_search(array, key):
+    return binary_search_helper(array, 0, len(array) - 1, key)
+
+
+def binary_search_helper(array, left, right, key):
+    if left > right:
+        return -1
+    if left == right:
+        if array[left] == key:
+            return left
+        else:
+            return -1
+    mid = (left + right) // 2
+    if array[mid] == key:
+        return binary_search_helper(array, left, mid, key)
+    elif array[mid] > key:
+        return binary_search_helper(array, left, mid - 1, key)
+    else:
+        return binary_search_helper(array, mid + 1, right, key)
 
 
 if __name__ == '__main__':
@@ -12,5 +28,7 @@ if __name__ == '__main__':
     input_queries = list(map(int, input().split()))
     assert len(input_queries) == num_queries
 
+    # input_keys = [2, 4, 4, 4, 7, 7, 9]
+    # input_queries = [9, 4, 5, 2]
     for q in input_queries:
         print(binary_search(input_keys, q), end=' ')
