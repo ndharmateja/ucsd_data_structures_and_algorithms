8946523c875e2620e51caabb8e707755a9e459e4
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-05-02 12:54:51 +0530

    Solve w5q2

--- course1_algorithmic_toolbox/week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
+++ course1_algorithmic_toolbox/week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
@@ -1,20 +1,23 @@
 # Uses python3
-import sys
+
 
 def optimal_sequence(n):
-    sequence = []
-    while n >= 1:
-        sequence.append(n)
-        if n % 3 == 0:
-            n = n // 3
-        elif n % 2 == 0:
-            n = n // 2
-        else:
-            n = n - 1
-    return reversed(sequence)
+    path = {1: {1}}
+    optimal_sequence_helper(n, path)
+    return path[n]
 
-input = sys.stdin.read()
-n = int(input)
+
+def optimal_sequence_helper(n, path):
+    for i in range(1, n):
+        if (i + 1 not in path) or (i + 1 in path and len(path[i + 1]) > len(path[i]) + 1):
+            path[i + 1] = [*path[i], i + 1]
+        if (i * 2 not in path) or (i * 2 in path and len(path[i * 2]) > len(path[i]) + 1):
+            path[i * 2] = [*path[i], i * 2]
+        if (i * 3 not in path) or (i * 3 in path and len(path[i * 3]) > len(path[i]) + 1):
+            path[i * 3] = [*path[i], i * 3]
+
+
+n = int(input())
 sequence = list(optimal_sequence(n))
 print(len(sequence) - 1)
 for x in sequence:
