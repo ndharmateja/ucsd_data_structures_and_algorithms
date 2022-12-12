11c3177e83790e98cc41d42e8deef64b46aee607
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-12-12 16:35:24 +0530

    Solve c2w1q3

--- course2_data_structures/week1_basic_data_structures/3_network_simulation/process_packages.py
+++ course2_data_structures/week1_basic_data_structures/3_network_simulation/process_packages.py
@@ -11,9 +11,42 @@ class Buffer:
         self.size = size
         self.finish_time = []
 
-    def process(self, request):
-        # write your code here
-        return Response(False, -1)
+    def peek(self):
+        return self.finish_time[-1]
+
+    def dequeue(self):
+        return self.finish_time.pop()
+
+    # Returns the start time of the enqueued request
+    def enqueue(self, request: Request):
+        # if the queue is empty, the packet will be processed
+        # at the time it arrives => start time = arrival time
+        # so finish time will be arrival time + process time
+
+        # if queue is not empty, the packet will be processed
+        # at the time when the last packet in the queue gets processed
+        request_start_time = request.arrived_at if len(
+            self.finish_time) == 0 else self.finish_time[0]
+
+        self.finish_time.insert(
+            0, request_start_time + request.time_to_process)
+
+        return request_start_time
+
+    def process(self, request: Request):
+        # Remove all the completed requests at the time this request comes
+        curr_time = request.arrived_at
+        while len(self.finish_time) > 0 and self.peek() <= curr_time:
+            self.dequeue()
+
+        # If the buffer is full, we cannot process the request
+        if len(self.finish_time) == self.size:
+            return Response(True, -1)
+
+        # Else, we add the finish time to the queue and
+        # return the response with current time
+        # (the time at which the request starts processing)
+        return Response(False, self.enqueue(request))
 
 
 def process_requests(requests, buffer):
