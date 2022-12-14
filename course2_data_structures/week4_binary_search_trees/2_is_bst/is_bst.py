fb0fc818ed27047b24e90c86eab20c16c953f98c
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-12-14 18:20:09 +0530

    Solve c2w4q2

--- course2_data_structures/week4_binary_search_trees/2_is_bst/is_bst.py
+++ course2_data_structures/week4_binary_search_trees/2_is_bst/is_bst.py
@@ -1,23 +1,46 @@
 #!/usr/bin/python3
 
-import sys, threading
+import sys
+import threading
 
-sys.setrecursionlimit(10**7) # max depth of recursion
-threading.stack_size(2**25)  # new thread will get stack of such size
+sys.setrecursionlimit(10**8)  # max depth of recursion
+threading.stack_size(2**26)  # new thread will get stack of such size
 
-def IsBinarySearchTree(tree):
-  # Implement correct algorithm here
-  return True
+
+def is_bst(tree):
+    if len(tree) == 0:
+        return True
+
+    two_31 = 2 ** 31
+    return is_bst_helper(tree, 0, -two_31, two_31 - 1)
+
+
+def is_bst_helper(tree, index, min, max):
+    if index == -1:
+        return True
+
+    [key, left_index, right_index] = tree[index]
+    if key < min or key > max:
+        return False
+
+    return is_bst_helper(tree, left_index, min, key - 1) and is_bst_helper(tree, right_index, key + 1, max)
 
 
 def main():
-  nodes = int(sys.stdin.readline().strip())
-  tree = []
-  for i in range(nodes):
-    tree.append(list(map(int, sys.stdin.readline().strip().split())))
-  if IsBinarySearchTree(tree):
-    print("CORRECT")
-  else:
-    print("INCORRECT")
+    nodes = int(sys.stdin.readline().strip())
+    tree = []
+    for i in range(nodes):
+        tree.append(list(map(int, sys.stdin.readline().strip().split())))
+    # tree = [
+    #     [4, 1, -1],
+    #     [2, 2, 3],
+    #     [1, -1, -1],
+    #     [5, -1, -1]
+    # ]
+    if is_bst(tree):
+        print("CORRECT")
+    else:
+        print("INCORRECT")
+
 
 threading.Thread(target=main).start()
