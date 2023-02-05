0a647e17deb8b4ade1982d44de9394a5dc56dbd3
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-02-05 14:39:38 +0530

    Solve c3w1q1

--- course3_algorithms_on_graphs/week1_graph_decomposition1/1_finding_exit_from_maze/Reachability.java
+++ course3_algorithms_on_graphs/week1_graph_decomposition1/1_finding_exit_from_maze/Reachability.java
@@ -1,18 +1,38 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Reachability {
-    private static int reach(ArrayList<Integer>[] adj, int x, int y) {
-        //write your code here
+    private static int reach(ArrayList<Integer>[] adj, int x, int y,
+            Set<Integer> visited) {
+        // Add node 'x' to the visited set
+        visited.add(x);
+
+        // Base case: If 'y' is found, we return 1
+        if (x == y)
+            return 1;
+
+        // For each unvisited neighbour of 'x', find
+        // if 'y' can be reached from the neighbour
+        // and return 1 if yes
+        for (int neighbour : adj[x]) {
+            if (!visited.contains(neighbour) && reach(adj, neighbour, y, visited) == 1)
+                return 1;
+        }
+
+        // If we reach here, 'y' can't be reached from 'x'
         return 0;
     }
-
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int m = scanner.nextInt();
-        ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
+
+        @SuppressWarnings("unchecked")
+        ArrayList<Integer>[] adj = (ArrayList<Integer>[]) new ArrayList[n];
+
         for (int i = 0; i < n; i++) {
             adj[i] = new ArrayList<Integer>();
         }
@@ -25,7 +45,9 @@ public class Reachability {
         }
         int x = scanner.nextInt() - 1;
         int y = scanner.nextInt() - 1;
-        System.out.println(reach(adj, x, y));
+        scanner.close();
+
+        Set<Integer> visited = new HashSet<>();
+        System.out.println(reach(adj, x, y, visited));
     }
 }
-
