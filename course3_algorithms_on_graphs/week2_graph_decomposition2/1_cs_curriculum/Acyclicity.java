377dbf5a3d36a68ba9e90135b8a2b630b308068f
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-10-04 00:59:33 +0530

    Solve c3w2q1

--- course3_algorithms_on_graphs/week2_graph_decomposition2/1_cs_curriculum/Acyclicity.java
+++ course3_algorithms_on_graphs/week2_graph_decomposition2/1_cs_curriculum/Acyclicity.java
@@ -1,9 +1,63 @@
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Acyclicity {
+
+    // returns true if there is a cycle while exploring
+    private static boolean explore(ArrayList<Integer>[] adj, int v, Set<Integer> visited,
+            Set<Integer> currentPath) {
+        // Add current vertex to the visited set
+        visited.add(v);
+
+        // Add current vertex to the path of exploration
+        currentPath.add(v);
+
+        // For each neighbour
+        for (int neighbour : adj[v]) {
+            // If the current path of exploration already contains the neighbour
+            // that means there is a cycle and we return true
+            if (currentPath.contains(neighbour)) {
+                return true;
+            }
+
+            // We explore an unvisited neighbour
+            // and if that call returns true
+            // it means that there is a cycle somewhere along the exploration of
+            // the neighbour and we return true
+            if (!visited.contains(neighbour)) {
+                if (explore(adj, neighbour, visited, currentPath)) {
+                    return true;
+                }
+            }
+        }
+
+        // Remove the current vertex from the current path
+        currentPath.remove(v);
+
+        // If there was a cycle (completed by a neighbour or
+        // during the exploration of a neighbour) we would have already
+        // returned true
+        // So if we reach here we can return false
+        return false;
+    }
+
     private static int acyclic(ArrayList<Integer>[] adj) {
-        //write your code here
+        // Explore each unvisited vertex
+        HashSet<Integer> visited = new HashSet<>();
+        for (int v = 0; v < adj.length; v++) {
+            if (!visited.contains(v)) {
+                // If exploration of current unvisited vertex returns true
+                // it means there is a cycle in this exploration
+                // and we return 1
+                if (explore(adj, v, visited, new HashSet<>())) {
+                    return 1;
+                }
+            }
+        }
+
+        // If we reach here that means no cycles and we return 0
         return 0;
     }
 
@@ -11,7 +65,8 @@ public class Acyclicity {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int m = scanner.nextInt();
-        ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
+        @SuppressWarnings("unchecked")
+        ArrayList<Integer>[] adj = (ArrayList<Integer>[]) new ArrayList[n];
         for (int i = 0; i < n; i++) {
             adj[i] = new ArrayList<Integer>();
         }
@@ -21,7 +76,7 @@ public class Acyclicity {
             y = scanner.nextInt();
             adj[x - 1].add(y - 1);
         }
+        scanner.close();
         System.out.println(acyclic(adj));
     }
 }
-
