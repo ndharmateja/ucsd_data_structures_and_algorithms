7d63a08ce05eb023f0259ed4b983e24651133878
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-10-05 00:37:03 +0530

    Solve c3w3q1

--- course3_algorithms_on_graphs/week3_paths_in_graphs1/1_flight_segments/BFS.java
+++ course3_algorithms_on_graphs/week3_paths_in_graphs1/1_flight_segments/BFS.java
@@ -5,7 +5,42 @@ import java.util.Scanner;
 
 public class BFS {
     private static int distance(ArrayList<Integer>[] adj, int s, int t) {
-        //write your code here
+        // Initialize all the distances to -1
+        // and the distance of s to 0
+        int[] distances = new int[adj.length];
+        for (int i = 0; i < distances.length; i++) {
+            distances[i] = -1;
+        }
+        distances[s] = 0;
+
+        // Initialize a queue and add the source
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(s);
+
+        // Run the algo while the queue is not empty
+        while (!queue.isEmpty()) {
+            // Get the current vertex for processing
+            int curr = queue.poll();
+
+            // Go through each undiscovered neighbour (distance is -1 if undiscovered)
+            // and add them to queue
+            for (int neighbour : adj[curr]) {
+                if (distances[neighbour] == -1) {
+                    queue.add(neighbour);
+
+                    // distance of neighbour is distance of curr vertex + 1
+                    distances[neighbour] = distances[curr] + 1;
+
+                    // If the neighbour is the destination node
+                    // we return its distance
+                    if (neighbour == t) {
+                        return distances[neighbour];
+                    }
+                }
+            }
+        }
+
+        // If t is present in the graph we never reach here
         return -1;
     }
 
@@ -13,7 +48,8 @@ public class BFS {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int m = scanner.nextInt();
-        ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
+        @SuppressWarnings("unchecked")
+        ArrayList<Integer>[] adj = (ArrayList<Integer>[]) new ArrayList[n];
         for (int i = 0; i < n; i++) {
             adj[i] = new ArrayList<Integer>();
         }
@@ -26,7 +62,7 @@ public class BFS {
         }
         int x = scanner.nextInt() - 1;
         int y = scanner.nextInt() - 1;
+        scanner.close();
         System.out.println(distance(adj, x, y));
     }
 }
-
